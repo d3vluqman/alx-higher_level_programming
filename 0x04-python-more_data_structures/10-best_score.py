a94ee7ef90644a145718269b8353469fc5b16bac
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-06-09 00:59:05 +0100

    best score 💯

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        score = None
-        person = None
-        for k in a_dictionary.keys():
-            if score is None or dictionary[i] > score:
-                score = a_dictionary[i]
-                person = i
-        return person
+    if not a_dictionary:
+        return None
+    return max(a_dictionary)
