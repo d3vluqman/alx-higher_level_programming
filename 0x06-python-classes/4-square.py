c02a46f44faa35b11aa8ee9534d3da535c935597
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-06-22 11:09:53 +0100

    update doc

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Square class."""
+
 
 class Square:
     """Square class."""
-
     def __init__(self, size=0):
         """__init__ method that sets the size of square.
         Args:
@@ -35,17 +36,3 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-
-if __name__ == '__main__':
-    my_square = Square(89)
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-    my_square.size = 3
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-    try:
-        my_square.size = "5 feet"
-        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-    except Exception as e:
-        print(e)
