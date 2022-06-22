453c3b870acafcb7d39d3c9fdd62af3316dcb351
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-06-22 11:13:47 +0100

    update doc

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Instantiation with optional size.
-    Public instance method: def area(self).
-    Public instance method: def my_print(self).
-    """
+"""Represents a square.
+Private instance attribute: size:
+- property def size(self)
+- property setter def size(self, value)
+Instantiation with optional size.
+Public instance method: def area(self).
+Public instance method: def my_print(self).
+"""
 
+
+class Square:
+    """square class"""
     def __init__(self, size=0):
         """Initializes the data."""
         self.__size = size
